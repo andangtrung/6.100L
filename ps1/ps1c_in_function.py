e8b5f75a541227d0a3b7ac897f4a319cf9b764a4
An Dang Trung <andang.code@gmail.com>
2025-10-18 04:07:42 +0700

    Problem Set 1 Part C
    Part C: Choosing an Interest Rate

--- ps1/ps1c_in_function.py
+++ ps1/ps1c_in_function.py
@@ -1,9 +1,33 @@
 def part_c(initial_deposit):
 	#########################################################################
-	
+	cost_of_dream_home = 800000
+	portion_down_payment = 0.25
+	cost_for_down_payment = cost_of_dream_home * portion_down_payment
+	months = 36
+	epsilon = 100
+	low = 0
+	high = 1
+	amount_saved = initial_deposit * (1 + high / 12) ** months
+	r = None
+	steps = 0
 	
 	##################################################################################################
 	## Determine the lowest rate of return needed to get the down payment for your dream home below ##
 	##################################################################################################
+	if amount_saved < cost_for_down_payment:
+	    pass
+	else:
+	    while abs(amount_saved - cost_for_down_payment) > epsilon:
+	        r = (high + low) / 2
+	        amount_saved = initial_deposit * (1 + r / 12) ** months
 	
+	        if amount_saved < cost_for_down_payment:
+	            low = r
+	        else:
+	            high = r
+	
+	        steps += 1
+	
+	print(f"Best saving rate: {r}")
+	print(f"Steps in bisection search: {steps}")
 	return r, steps
